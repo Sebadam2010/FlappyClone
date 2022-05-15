a1512edefc7d6b69007dcd06a5eb758ba6fe2844
Sebastian A <Sebadam2010@hotmail.co.uk>
2022-05-15 15:19:57 +0100

    Toggle FPS counter works

---
+++ Assets/Scripts/GraphyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphyScript : MonoBehaviour
+{
+    public static GraphyScript graphy_script { get; private set; }
+
+    private void Awake()
+    {
+     
+
+    }
+
+    private void Start()
+    {
+        graphy_script = this;
+    }
+}
